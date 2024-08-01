13ce6831b3697513e2306bfdf297d598f312dc61
tiger98 <biguptiger@gmail.com>
2024-08-01 09:32:07 +0900

    Practice of State

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,52 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
-void main() => runApp(MyApp()); 
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) { 
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: RandomWords(),
-    );
-  }
-}
-
-// StatefulWidgetを継承するRandomWordsクラス(Widget)を作成
-class RandomWords extends StatefulWidget {
-  @override
-  // RandomWordsStateクラスのインスタンスを返す
-  RandomWordsState createState() => RandomWordsState();
-}
-
-// createState()で返されるRandomWordsStateクラスを定義する
-class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-
-// ListViewクラスのインスタンスを返す
-  Widget _buildSuggestions() {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i){
-        // 偶数行の場合は_suggestionsリストに新しい単語を追加
-        // 奇数行の場合はDividerクラスのインスタンスを返す
-        if(i.isOdd) return Divider();
-        final index = i ~/ 2;
-        if(index >= _suggestions.length){
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        // _buildRowメソッドの呼び出し
-        return _buildRow(_suggestions[index]);
-      }
-    );
-  }
-// ListTileクラスのインスタンスを返す
-  Widget _buildRow(WordPair pair) {
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    // Textクラスのインスタンスを返す
-    // 引数はランダムな英単語を生成するWordPairクラスのasPascalCaseプロパティ
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Randoom!'),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: MyWidget(),
+        ),
       ),
-      body: _buildSuggestions(),
     );
   }
 }
 
+// StatefulWidgetを継承するとStateを扱える
+// このWidgetを表示すると、Stateを元にUIが作成される
+class MyWidget extends StatefulWidget {
+  // 使用するStateを指定
+  @override
+  _MyWidgetState createState() => _MyWidgetState();
+}
+
+// Stateを継承して使う
+class _MyWidgetState extends State<MyWidget> {
+  // データを宣言
+  int count = 0;
+
+  // データを元にWidgetを作る
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(count.toString()),
+        TextButton(
+          onPressed: () {
+            setState(() {
+              count = count + 1;
+            });
+          },
+          child: Text('count Up'),
+        ),
+      ],
+    );
+}
+}
