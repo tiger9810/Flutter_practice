27cd45afd275144a2a1d8a7817b6f1659fc43b0f
tiger98 <biguptiger@gmail.com>
2024-08-01 16:54:41 +0900

    画面遷移と値の受け渡し

--- lib/main.dart
+++ lib/main.dart
@@ -9,44 +9,108 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.blue),
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: MyWidget(),
-        ),
-      ),
+      home: TodoListPage(),
     );
   }
 }
 
-// StatefulWidgetを継承するとStateを扱える
-// このWidgetを表示すると、Stateを元にUIが作成される
-class MyWidget extends StatefulWidget {
-  // 使用するStateを指定
+class TodoListPage extends StatefulWidget {
   @override
-  _MyWidgetState createState() => _MyWidgetState();
+  _TodoListPageState createState() => _TodoListPageState();
 }
 
-// Stateを継承して使う
-class _MyWidgetState extends State<MyWidget> {
-  // データを宣言
-  int count = 0;
+// list一覧用Widget
+class _TodoListPageState extends State<TodoListPage> {
+    List<String> todoList = [];
 
-  // データを元にWidgetを作る
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text(count.toString()),
-        TextButton(
-          onPressed: () {
-            setState(() {
-              count = count + 1;
-            });
-          },
-          child: Text('count Up'),
-        ),
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('all List')
+      ),
+      body:ListView.builder(
+        itemCount: todoList.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(title: Text(todoList[index])
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final newListText = await Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context){
+              return TodoAppPage();
+            }),
+        );
+        if (newListText != null) {
+          setState(() {
+            todoList.add(newListText);
+          });
+        }
+      },
+      child: Icon(Icons.add),
+    ),
     );
+  }
 }
+
+// TodoAppPageをStatefulWidgetにする
+class TodoAppPage extends StatefulWidget {
+  @override
+  _TodoAddPageState createState() => _TodoAddPageState();
+}
+
+class _TodoAddPageState extends State<TodoAppPage> {
+  String _text = "";
+
+  @override
+  Widget build(BuildContext context){
+   return Scaffold(
+    appBar: AppBar(
+      title:Text('AddPage'),
+      automaticallyImplyLeading: false,
+      ),
+    body: Container(
+      padding: EdgeInsets.all(64),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(_text, style: TextStyle(color: Colors.amber)),
+          
+          const SizedBox(height: 8),
+          TextField(
+            onChanged: (String value) {
+              setState(() {
+                _text = value;
+              });
+            },
+          ),
+          Container(
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: (){
+                Navigator.of(context).pop(_text);
+              },
+              child: Text('Add list')
+            ),
+          ),
+          const SizedBox(height: 8),
+          Container(
+            width: double.infinity,
+            child: TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('cancel')
+            ),
+            )
+        ],
+      )
+    )
+   );
+  }
 }
